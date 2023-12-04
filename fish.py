5ba366c9d78aa9216d778130bca3a469ba1038e6
bankkkay <qwertoz322@gmail.com>
2023-12-04 19:33:19 +0700

    change

---
+++ fish.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class Habitat(Enum):
+    OCEAN = "OCEAN"
+    RIVER = "RIVER"
+    SEA = "SEA"
+
+class Fish:
+    def __init__(self, name, habitat):
+        self.name = name
+        if habitat in [item.value for item in Habitat]:
+            self.habitat = habitat
+        else:
+            raise ValueError(f"Невозможный ареал обитания для рыбы ({name})")
+
+    def __str__(self):
+        return f"FISH, {self.name}, {self.habitat}"
--- main.py
+++ main.py
@@ -3,23 +3,6 @@ import datetime
 import logging
 import math
 
-class Habitat(Enum):
-    OCEAN = "OCEAN"
-    RIVER = "RIVER"
-    SEA = "SEA"
-
-
-class Fish:
-    def __init__(self, name, habitat):
-        self.name = name
-        if habitat in [item.value for item in Habitat]:
-            self.habitat = habitat
-        else:
-            raise ValueError(f"Невозможный ареал обитания для рыбы ({name})")
-
-    def __str__(self):
-        return f"FISH, {self.name}, {self.habitat}"
-
 
 class Bird:
     invalid_speed_values = set()
